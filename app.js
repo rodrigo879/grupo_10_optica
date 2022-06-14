7ac66d4deba91f3b197b818ec483d40a2862fc53
Rodrigo Alvarez <ralvarez@lequipe.net>
2022-06-14 13:14:17 -0300

    index movile terminado

--- app.js
+++ app.js
@@ -5,23 +5,27 @@ const app = express();
 app.use(express.static(path.resolve(__dirname,'./public')));
 
 app.get('/', function(req,res){
-    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+    res.sendFile(path.resolve(__dirname,'./views/index.html'))
+})
+
+app.get('/base.html', function(req,res){
+    res.sendFile(path.resolve(__dirname,'./views/base.html'))
 })
 
 app.get('/login.html', function(req,res){
     res.sendFile(path.resolve(__dirname,'./views/login.html'))
 })
 
-app.get('/login.html', function(req,res){
+app.get('/register.html', function(req,res){
     res.sendFile(path.resolve(__dirname,'./views/register.html'))
 })
 
-app.get('/login.html', function(req,res){
-    res.sendFile(path.resolve(__dirname,'./views/details_product.html'))
+app.get('/productDetail.html', function(req,res){
+    res.sendFile(path.resolve(__dirname,'./views/productDetail.html'))
 })
 
-app.get('/login.html', function(req,res){
-    res.sendFile(path.resolve(__dirname,'./views/cart.html'))
+app.get('/productCart.html', function(req,res){
+    res.sendFile(path.resolve(__dirname,'./views/productCart.html'))
 })
 
 app.listen(3000, function(){
