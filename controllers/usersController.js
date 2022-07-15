798ee91099ea12f6c7938b10fef883281a878899
Fran-chess <franleonardo33@gmail.com>
2022-07-14 21:18:50 -0300

    creando base de datos de usuarios

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,39 @@
+const usersJson = require('../database/jsonTable');
+const usersModel = usersJson('users')
+
 const userController = {
     login: (req, res) => {
         res.render('./users/login');
     },
+    logged: (req, res) => {
+        let userBody = req.body.user;
+        let users = usersModel.readFile('users.json')
+        let userFilter = users.filter(person => person.user == userBody)
+        if(userFilter.length > 0){  
+            let userPassword = req.body.password;
+            if(userFilter.password == userPassword){
+                res.redirect('/')
+            } else {
+                res.send('Datos incorrectos')
+            }
+        } else {
+            res.send('Datos incorrectos') 
+
+        }
+    },
     register: (req, res) => {
         res.render('./users/register');
+    },
+    create: (req, res) => {
+        let users = req.body;
+        users.imageUser = req.file.filename;
+        users.admin = false;
+        let userId = usersModel.create(users);
+        
+
+        res.redirect('/')
     }
+
 }
 
 module.exports = userController;
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const router = express.Router();
-
+const multer = require('multer');
 const usersController = require('../controllers/usersController')
+const path = require('path')
+
+// Para cargar las imagenes de productos, donde se van a cargar y con que nombre
+const storage = multer.diskStorage({ 
+    destination: (req,file,cb) => {
+       cb(null, path.join(__dirname, '../public/images/users'));
+    },
+    filename: (req,file,cb) => {
+        let newFileName = 'user-' + Date.now() + path.extname(file.originalname);
+        cb(null, newFileName);
+    }
+});
+
+// Cargar la imagen
+const upload = multer({ storage })
+
 
 
 router.get('/login', usersController.login);
+router.post('/login', usersController.logged);
+
+
+
 router.get('/register', usersController.register);
+router.post('/register', upload.single('imageUser'), usersController.create);
+
+
+
 
 module.exports = router;
