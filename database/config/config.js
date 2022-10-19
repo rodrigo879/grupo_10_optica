25f0a0b6d33e2e940ae2c5b82d1514057e82e0ae
Fran-chess <franleonardo33@gmail.com>
2022-10-19 08:40:59 -0300

    Proyecto terminado(grupo-10)

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "5826",
+    "password": "",
     "database": "optica_riff",
     "host": "127.0.0.1",
     "dialect": "mysql"
